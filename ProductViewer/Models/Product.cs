2913b754ee2077c731a44659fbccab68aaa72587
Suman Palikhe <palikhe.suman@gmail.com>
2020-05-04 17:02:55 +1000

    added models

---
+++ ProductViewer/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductViewer.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+    }
+}
---
+++ ProductViewer/Models/RetailerProduct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProductViewer.Models
+{
+    public class RetailerProduct
+    {
+        public int ProductId { get; set; }
+
+        public string RetailerName { get; set; }
+
+        public string RetailerProductCode { get; set; }
+
+        public string RetailerProductCodeType { get; set; }
+
+        public DateTime DateReceived { get; set; }
+    }
+}
